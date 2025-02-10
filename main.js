009188dd4c109a5e2a92c2b2f65ef964d0249a21
julia-martynovych <inquam414@gmail.com>
2025-02-10 14:01:03 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// const listadoPlanetas = ['Narnia', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno', 'Pluton', 'Mercurio']
+// listadoPlanetas.shift();
+// const planetaMercurio = listadoPlanetas.pop();
+// listadoPlanetas.unshift(planetaMercurio);
+// const planetaPluton = listadoPlanetas.pop();
+// console.log(listadoPlanetas);
+
+// const buttonGreet = document.getElementById("button_greet")
+
+// console.log(buttonGreet)
+
+// buttonGreet.addEventListener("click", function () {
+//     console.log("Hola desde botton");
+//     alert("Hola. Esto es un alert");
+// }
+// )
+
+//*Crea una función para felicitar el cumpleaños que pueda
+//  darte respuesta en tres idiomas ( español, inglés y Alemán) 
+// y que tenga una respuesta con iconos, en el caso de que no sea
+//  ninguno de los tres idiomas🎂.*/
+
+// function happyBirthday(name, language){
+//     switch (language) {
+//         case "es": console.log("Feliz cumpleaños, " + name);
+//             break; 
+//         case "en": console.log("Happy birthday, " + name);
+//             break;
+//         case "de": console.log("Alles Gute zum Geburtstag, " + name);
+//             break;
+//          default:  console.log("🎂, " + name)
+// }
+// };
+
+// happyBirthday("Maria", "de");
+
+// function dice(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+// console.log(dice(1, 6))
+
+
+// function dice() {
+//   let aleatoryNumber = Math.floor(Math.random() * 6) + 1;
+//   return aleatoryNumber
+// }
+// // console.log(Math.floor(Math.random() * 6) + 1);
+// console.log(dice())
